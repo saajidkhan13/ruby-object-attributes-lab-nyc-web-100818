f145e3fb723cc2f43aa8dd049b5e6ede21be9a5e
Saajid Khan <saajid.khan13@stjohns.edu>
2018-10-09 18:06:24 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name
+    @name
+  end
+
+  def name= (dog_name)
+    name = @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed= (dog_breed)
+    breed = @breed
+  end
+
+end
